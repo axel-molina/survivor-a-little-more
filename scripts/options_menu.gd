081a8c27a3ecc8cc8c453810cebeb86a98d48fcb
Axel Molina <ivanmolina052@gmail.com>
2026-08-20 19:55:24 -0300

    Polish Options Menu visual design: Inika font, blur shader, gold accents, styled controls, and smooth animations

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -1,32 +1,40 @@
 extends CanvasLayer
 
-@onready var bg_rect: ColorRect = $ColorRect
-@onready var tabs: TabContainer = $CenterContainer/PanelContainer/HBoxContainer/TabContainer
+@onready var blur_overlay: ColorRect = $BlurOverlay
+@onready var panel_container: PanelContainer = $CenterContainer/PanelContainer
+@onready var tabs: TabContainer = $CenterContainer/PanelContainer/MainLayout/ContentLayout/TabContainer
 
-@onready var btn_video: Button = $CenterContainer/PanelContainer/HBoxContainer/Sidebar/BtnVideo
-@onready var btn_audio: Button = $CenterContainer/PanelContainer/HBoxContainer/Sidebar/BtnAudio
-@onready var btn_general: Button = $CenterContainer/PanelContainer/HBoxContainer/Sidebar/BtnGeneral
-@onready var btn_controls: Button = $CenterContainer/PanelContainer/HBoxContainer/Sidebar/BtnControls
-@onready var btn_back: Button = $CenterContainer/PanelContainer/HBoxContainer/Sidebar/BtnBack
+@onready var btn_video: Button = $CenterContainer/PanelContainer/MainLayout/ContentLayout/Sidebar/BtnVideo
+@onready var btn_audio: Button = $CenterContainer/PanelContainer/MainLayout/ContentLayout/Sidebar/BtnAudio
+@onready var btn_general: Button = $CenterContainer/PanelContainer/MainLayout/ContentLayout/Sidebar/BtnGeneral
+@onready var btn_controls: Button = $CenterContainer/PanelContainer/MainLayout/ContentLayout/Sidebar/BtnControls
+@onready var btn_back: Button = $CenterContainer/PanelContainer/MainLayout/ContentLayout/Sidebar/BtnBack
 
 # Controles Video
-@onready var chk_fullscreen: CheckButton = $CenterContainer/PanelContainer/HBoxContainer/TabContainer/VideoTab/Grid/ChkFullscreen
-@onready var opt_resolution: OptionButton = $CenterContainer/PanelContainer/HBoxContainer/TabContainer/VideoTab/Grid/OptResolution
-@onready var opt_fps: OptionButton = $CenterContainer/PanelContainer/HBoxContainer/TabContainer/VideoTab/Grid/OptFPS
+@onready var chk_fullscreen: CheckButton = $CenterContainer/PanelContainer/MainLayout/ContentLayout/TabContainer/VideoTab/Grid/ChkFullscreen
+@onready var opt_resolution: OptionButton = $CenterContainer/PanelContainer/MainLayout/ContentLayout/TabContainer/VideoTab/Grid/OptResolution
+@onready var opt_fps: OptionButton = $CenterContainer/PanelContainer/MainLayout/ContentLayout/TabContainer/VideoTab/Grid/OptFPS
 
 # Controles Audio
-@onready var sld_master: HSlider = $CenterContainer/PanelContainer/HBoxContainer/TabContainer/AudioTab/VBox/SldMaster
-@onready var sld_music: HSlider = $CenterContainer/PanelContainer/HBoxContainer/TabContainer/AudioTab/VBox/SldMusic
-@onready var sld_sfx: HSlider = $CenterContainer/PanelContainer/HBoxContainer/TabContainer/AudioTab/VBox/SldSFX
+@onready var sld_master: HSlider = $CenterContainer/PanelContainer/MainLayout/ContentLayout/TabContainer/AudioTab/VBox/MasterGroup/SldMaster
+@onready var val_master: Label = $CenterContainer/PanelContainer/MainLayout/ContentLayout/TabContainer/AudioTab/VBox/MasterGroup/HeaderMaster/ValMaster
+
+@onready var sld_music: HSlider = $CenterContainer/PanelContainer/MainLayout/ContentLayout/TabContainer/AudioTab/VBox/MusicGroup/SldMusic
+@onready var val_music: Label = $CenterContainer/PanelContainer/MainLayout/ContentLayout/TabContainer/AudioTab/VBox/MusicGroup/HeaderMusic/ValMusic
+
+@onready var sld_sfx: HSlider = $CenterContainer/PanelContainer/MainLayout/ContentLayout/TabContainer/AudioTab/VBox/SFXGroup/SldSFX
+@onready var val_sfx: Label = $CenterContainer/PanelContainer/MainLayout/ContentLayout/TabContainer/AudioTab/VBox/SFXGroup/HeaderSFX/ValSFX
 
 # Controles General
-@onready var opt_language: OptionButton = $CenterContainer/PanelContainer/HBoxContainer/TabContainer/GeneralTab/VBox/HBoxLang/OptLanguage
+@onready var opt_language: OptionButton = $CenterContainer/PanelContainer/MainLayout/ContentLayout/TabContainer/GeneralTab/VBox/HBoxLang/OptLanguage
 
 # SFX
 @export var hover_sfx: AudioStream = preload("res://sfx/ui/hover_sound.mp3")
 @export var submit_sfx: AudioStream = preload("res://sfx/ui/submit_sound.wav")
 var _hover_player: AudioStreamPlayer
 var _submit_player: AudioStreamPlayer
+
+var _tab_buttons: Array[Button] = []
 
 var available_resolutions = [
 	Vector2i(1280, 720),
@@ -42,12 +50,12 @@ signal closed
 
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	
+	_tab_buttons = [btn_video, btn_audio, btn_general, btn_controls]
+	
 	_setup_sfx()
 	_populate_dropdowns()
 	_load_current_values()
-	
-	# Ocultar tabs (las pestañas visuales nativas)
-	tabs.tabs_visible = false
 	
 	# Conexiones Sidebar
 	btn_video.pressed.connect(func(): _switch_tab(0))
@@ -62,18 +70,28 @@ func _ready() -> void:
 	opt_fps.item_selected.connect(_on_fps_selected)
 	
 	# Conexiones UI Audio
-	sld_master.value_changed.connect(func(v): SettingsManager.set_volume("Master", v))
-	sld_music.value_changed.connect(func(v): SettingsManager.set_volume("Music", v))
-	sld_sfx.value_changed.connect(func(v): SettingsManager.set_volume("SFX", v))
+	sld_master.value_changed.connect(_on_master_volume_changed)
+	sld_music.value_changed.connect(_on_music_volume_changed)
+	sld_sfx.value_changed.connect(_on_sfx_volume_changed)
 	
 	# Conexiones UI General
 	opt_language.item_selected.connect(_on_language_selected)
 	
-	# Suscribirse al cambio de idioma (para actualizar textos dinámicamente si es necesario)
-	SettingsManager.settings_applied.connect(_on_settings_applied)
-	
 	# Iniciar en pestaña de video
 	_switch_tab(0)
+	
+	# Animación de entrada suave
+	_animate_open()
+
+
+func _animate_open() -> void:
+	panel_container.modulate.a = 0.0
+	panel_container.scale = Vector2(0.96, 0.96)
+	panel_container.pivot_offset = panel_container.size / 2.0
+	
+	var tween := create_tween().set_parallel(true).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	tween.tween_property(panel_container, "modulate:a", 1.0, 0.2)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, 0.2)
 
 
 func _setup_sfx() -> void:
@@ -89,12 +107,26 @@ func _setup_sfx() -> void:
 	_submit_player.bus = &"Master"
 	add_child(_submit_player)
 	
-	# Conectar hover
-	var all_buttons = [btn_video, btn_audio, btn_general, btn_controls, btn_back, chk_fullscreen]
-	for b in all_buttons:
-		b.mouse_entered.connect(_hover_player.play)
-		b.focus_entered.connect(_hover_player.play)
-		b.pressed.connect(_submit_player.play)
+	_connect_buttons_recursive(panel_container)
+
+
+func _connect_buttons_recursive(node: Node) -> void:
+	if node is Button:
+		node.mouse_entered.connect(_play_hover)
+		node.focus_entered.connect(_play_hover)
+		node.pressed.connect(_play_submit)
+	for child in node.get_children():
+		_connect_buttons_recursive(child)
+
+
+func _play_hover() -> void:
+	if _hover_player and hover_sfx:
+		_hover_player.play()
+
+
+func _play_submit() -> void:
+	if _submit_player and submit_sfx:
+		_submit_player.play()
 
 
 func _populate_dropdowns() -> void:
@@ -106,23 +138,24 @@ func _populate_dropdowns() -> void:
 	for i in range(available_resolutions.size()):
 		var res = available_resolutions[i]
 		if res.x <= screen_res.x and res.y <= screen_res.y:
-			opt_resolution.add_item(str(res.x) + "x" + str(res.y))
+			opt_resolution.add_item(str(res.x) + " x " + str(res.y))
 			opt_resolution.set_item_metadata(i, res)
-			if res == screen_res: added_current = true
+			if res == screen_res:
+				added_current = true
 			
 	if not added_current and not available_resolutions.has(screen_res):
-		opt_resolution.add_item(str(screen_res.x) + "x" + str(screen_res.y))
+		opt_resolution.add_item(str(screen_res.x) + " x " + str(screen_res.y))
 		opt_resolution.set_item_metadata(opt_resolution.item_count - 1, screen_res)
 
 	# FPS
 	opt_fps.clear()
-	opt_fps.add_item("30", 0)
+	opt_fps.add_item("30 FPS", 0)
 	opt_fps.set_item_metadata(0, 30)
-	opt_fps.add_item("60", 1)
+	opt_fps.add_item("60 FPS", 1)
 	opt_fps.set_item_metadata(1, 60)
-	opt_fps.add_item("120", 2)
+	opt_fps.add_item("120 FPS", 2)
 	opt_fps.set_item_metadata(2, 120)
-	opt_fps.add_item("144", 3)
+	opt_fps.add_item("144 FPS", 3)
 	opt_fps.set_item_metadata(3, 144)
 	opt_fps.add_item("Ilimitado", 4)
 	opt_fps.set_item_metadata(4, 0)
@@ -139,7 +172,10 @@ func _load_current_values() -> void:
 	var c = SettingsManager.config
 	
 	# Video
-	chk_fullscreen.button_pressed = c.get_value(SettingsManager.SECTION_VIDEO, "fullscreen", true)
+	var is_fs = c.get_value(SettingsManager.SECTION_VIDEO, "fullscreen", true)
+	chk_fullscreen.button_pressed = is_fs
+	opt_resolution.disabled = is_fs
+	
 	var res_w = c.get_value(SettingsManager.SECTION_VIDEO, "resolution_w", 1920)
 	var res_h = c.get_value(SettingsManager.SECTION_VIDEO, "resolution_h", 1080)
 	var current_res = Vector2i(res_w, res_h)
@@ -155,9 +191,17 @@ func _load_current_values() -> void:
 			break
 			
 	# Audio
-	sld_master.value = c.get_value(SettingsManager.SECTION_AUDIO, "master", 1.0)
-	sld_music.value = c.get_value(SettingsManager.SECTION_AUDIO, "music", 1.0)
-	sld_sfx.value = c.get_value(SettingsManager.SECTION_AUDIO, "sfx", 1.0)
+	var m_vol = c.get_value(SettingsManager.SECTION_AUDIO, "master", 1.0)
+	sld_master.value = m_vol
+	val_master.text = "%d%%" % int(round(m_vol * 100.0))
+	
+	var mu_vol = c.get_value(SettingsManager.SECTION_AUDIO, "music", 1.0)
+	sld_music.value = mu_vol
+	val_music.text = "%d%%" % int(round(mu_vol * 100.0))
+	
+	var sfx_vol = c.get_value(SettingsManager.SECTION_AUDIO, "sfx", 1.0)
+	sld_sfx.value = sfx_vol
+	val_sfx.text = "%d%%" % int(round(sfx_vol * 100.0))
 	
 	# General
 	var lang = c.get_value(SettingsManager.SECTION_GENERAL, "language", "es")
@@ -169,11 +213,19 @@ func _load_current_values() -> void:
 
 func _switch_tab(index: int) -> void:
 	tabs.current_tab = index
+	
+	# Actualizar estilos visuales de botones laterales
+	for i in range(_tab_buttons.size()):
+		var btn = _tab_buttons[i]
+		if i == index:
+			btn.add_theme_color_override("font_color", Color(1.0, 0.88, 0.45, 1.0))
+		else:
+			btn.remove_theme_color_override("font_color")
 
 
 func _on_fullscreen_toggled(toggled_on: bool) -> void:
 	SettingsManager.set_fullscreen(toggled_on)
-	opt_resolution.disabled = toggled_on # No se puede cambiar resolución si es pantalla completa
+	opt_resolution.disabled = toggled_on
 
 
 func _on_resolution_selected(index: int) -> void:
@@ -186,20 +238,32 @@ func _on_fps_selected(index: int) -> void:
 	SettingsManager.set_fps_limit(fps)
 
 
+func _on_master_volume_changed(value: float) -> void:
+	val_master.text = "%d%%" % int(round(value * 100.0))
+	SettingsManager.set_volume("Master", value)
+
+
+func _on_music_volume_changed(value: float) -> void:
+	val_music.text = "%d%%" % int(round(value * 100.0))
+	SettingsManager.set_volume("Music", value)
+
+
+func _on_sfx_volume_changed(value: float) -> void:
+	val_sfx.text = "%d%%" % int(round(value * 100.0))
+	SettingsManager.set_volume("SFX", value)
+
+
 func _on_language_selected(index: int) -> void:
 	var lang = opt_language.get_item_metadata(index) as String
 	SettingsManager.set_language(lang)
 
 
-func _on_settings_applied() -> void:
-	# Podríamos forzar actualización de UI si algo cambia externamente
-	pass
-
-
 func close() -> void:
+	_submit_player.play()
+	var tween := create_tween().set_parallel(true).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+	tween.tween_property(panel_container, "modulate:a", 0.0, 0.15)
+	tween.tween_property(panel_container, "scale", Vector2(0.95, 0.95), 0.15)
+	await tween.finished
 	visible = false
 	closed.emit()
 	queue_free()
-
-func open() -> void:
-	visible = true
